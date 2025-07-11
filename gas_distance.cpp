f6407af7ca34b060236e51dce3feb9966d8faee2
bfitz-vccs <bf2877@email.vccs.edu>
2025-07-10 23:35:00 -0400

    Created the gas_distance program and compiled it.

---
+++ gas_distance.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main(){
+    
+    std::cout << "The car can drive " << 20*23.5 << " miles when driven in town." << std::endl;
+    std::cout << "The car can drive " << 20*28.9 << " miles when driven on the highway." << std::endl;
+
+    return 0;
+}

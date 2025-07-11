aa664820cbf8c0708278d9ca5f886c6a5778b36a
bfitz-vccs <bf2877@email.vccs.edu>
2025-07-10 23:44:16 -0400

    Created the energy_drinks program and compiled it.

---
+++ energy_drinks.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main(){
+    
+    std::cout << "Around " << 16500*0.15 << " of the surveyed customers drink one or more energy drinks per week." << std::endl;
+    std::cout << "Around " << 16500*0.58 << " of the surveyed customers prefer citrus-flavored energy drinks." << std::endl;
+
+    return 0;
+}

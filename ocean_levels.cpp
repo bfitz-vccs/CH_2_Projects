5ae9176805470c320c0bb68e3c4816bb2e343738
bfitz-vccs <bf2877@email.vccs.edu>
2025-07-10 23:32:41 -0400

    Created the ocean_levels program and compiled it.

---
+++ ocean_levels.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main() {
+
+    std::cout << "The ocean will be " << 1.5*5 << " milimeters higher in five years time." << std::endl;
+    std::cout << "The ocean will be " << 1.5*7 << " milimeters higher in five years time." << std::endl;
+    std::cout << "The ocean will be " << 1.5*10 << " milimeters higher in five years time." << std::endl;
+    return 0;
+}

2e1ac543de159ddbaeacd5f71ab2ff5d584fd749
bfitz-vccs <bf2877@email.vccs.edu>
2025-07-11 23:49:33 -0400

    Created variables for the data values in case future edits or functionality is needed.

--- ocean_levels.cpp
+++ ocean_levels.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
 
 int main() {
+    int time_1 = 5;
+    int time_2 = 7;
+    int time_3 = 10;
+    float level_increase = 1.5;
 
-    std::cout << "The ocean will be " << 1.5*5 << " milimeters higher in five years time." << std::endl;
-    std::cout << "The ocean will be " << 1.5*7 << " milimeters higher in five years time." << std::endl;
-    std::cout << "The ocean will be " << 1.5*10 << " milimeters higher in five years time." << std::endl;
+    std::cout << "The ocean will be " << level_increase * time_1 << " milimeters higher in five years time." << std::endl;
+    std::cout << "The ocean will be " << level_increase * time_2 << " milimeters higher in five years time." << std::endl;
+    std::cout << "The ocean will be " << level_increase * time_3 << " milimeters higher in five years time." << std::endl;
     return 0;
 }

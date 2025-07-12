5216c12c63d1caa38c926917fcfb9d6725adf312
bfitz-vccs <bf2877@email.vccs.edu>
2025-07-12 00:57:11 -0400

    Fixed issue with outputting correct years.

--- ocean_levels.cpp
+++ ocean_levels.cpp
@@ -6,8 +6,8 @@ int main() {
     int time_3 = 10;
     float level_increase = 1.5;
 
-    std::cout << "The ocean will be " << level_increase * time_1 << " milimeters higher in five years time." << std::endl;
-    std::cout << "The ocean will be " << level_increase * time_2 << " milimeters higher in five years time." << std::endl;
-    std::cout << "The ocean will be " << level_increase * time_3 << " milimeters higher in five years time." << std::endl;
+    std::cout << "The ocean will be " << level_increase * time_1 << " milimeters higher in " << time_1 << " years time." << std::endl;
+    std::cout << "The ocean will be " << level_increase * time_2 << " milimeters higher in " << time_2 << " years time." << std::endl;
+    std::cout << "The ocean will be " << level_increase * time_3 << " milimeters higher in " << time_3 << " years time." << std::endl;
     return 0;
 }

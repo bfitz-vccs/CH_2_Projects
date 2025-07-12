a01eff649027c683f52969dba76aed68834dfa18
bfitz-vccs <bf2877@email.vccs.edu>
2025-07-11 23:54:18 -0400

    Created variables for the data values in case future edits or functionality is needed.

--- gas_distance.cpp
+++ gas_distance.cpp
@@ -2,8 +2,12 @@
 
 int main(){
     
-    std::cout << "The car can drive " << 20*23.5 << " miles when driven in town." << std::endl;
-    std::cout << "The car can drive " << 20*28.9 << " miles when driven on the highway." << std::endl;
+    int tank_size = 20;
+    float town_mileage = 23.5;
+    float highway_mileage = 28.9;
+
+    std::cout << "The car can drive " << tank_size * town_mileage << " miles when driven in town." << std::endl;
+    std::cout << "The car can drive " << tank_size * highway_mileage << " miles when driven on the highway." << std::endl;
 
     return 0;
 }
